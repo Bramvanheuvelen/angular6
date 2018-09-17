042de5beeace6a6c2c172b2eb6333b0fff419b15
Bram van Heuvelen <bramvanheuvelen@Brams-MacBook-Pro.local>
2018-09-17 10:09:25 +0200

    storing in firebase working

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -2,9 +2,11 @@ import { Recipe } from './recipes.model';
 import { Injectable } from '@angular/core';
 import { Ingredients } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -24,10 +26,14 @@ export class RecipeService {
                 new Ingredients('Meat', 20)
             ])
       ];
+constructor(private slService: ShoppingListService) { }
 
-      constructor(private slService: ShoppingListService) { }
+setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+      }
 
-    getRecipes() {
+getRecipes() {
         return this.recipes.slice();
     }
 
@@ -37,5 +43,20 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredients[  ]) {
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+      this.recipes.push(recipe);
+      this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+      this.recipes[index] = newRecipe;
+      this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+      this.recipes.splice(index, 1);
+      this.recipesChanged.next(this.recipes.slice());
     }
 }
