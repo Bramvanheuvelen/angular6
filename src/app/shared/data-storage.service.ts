14d72ee1121fb4a42f38c945a28f19e8c1d7b9ca
Bram van Heuvelen <bramvanheuvelen@Brams-MacBook-Pro.local>
2018-09-17 14:46:18 +0200

    authentication working

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { RecipeService } from '../recipes/recipes.service';
 import { Recipe } from '../recipes/recipes.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) {}
 
     storeRecipes() {
-      return this.http.put('https://recipebook-2280d.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+      const token = this.authService.getToken();
+      return this.http.put('https://recipebook-2280d.firebaseio.com/recipes.json?=auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-      this.http.get('https://recipebook-2280d.firebaseio.com/recipes.json')
+      const token = this.authService.getToken();
+      this.http.get('https://recipebook-2280d.firebaseio.com/recipes.json?auth=' + token)
         .map(
           (response: Response) => {
             const recipes: Recipe[] = response.json();
